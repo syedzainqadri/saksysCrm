9d3883f158086874bc7bb7f3abab7b6dba6fe880
EwigLife <96788793+syedzainqadri@users.noreply.github.com>
2023-12-06 21:41:41 +0500

    cache added

---
+++ bootstrap/cache/guide_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Guide\\Providers\\GuideServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Guide\\Providers\\GuideServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
